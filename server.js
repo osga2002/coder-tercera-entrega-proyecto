d11fe6c526cd067edbbcf4eeae238c66a52ebe57
sistemaschoho <sistemas@chohocolombia.com>
2022-05-18 00:38:44 -0500

    Se avanza en el entregable login

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+
+/** Inicia el servidor */
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(session({
+    secret: 'asdfghjklñ',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000
+    }
+}))
+
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
 app.get('/',(req, res) => {
-    res.send('Hello World from Heroku')
+    res.send('Tercera Entrega Proyecto Final')
 })
 
 const server = app.listen(port, ()=> {
@@ -11,5 +32,5 @@ const server = app.listen(port, ()=> {
 })
 
 server.on("error", error  => {
-    console.log("this is the ERROR", error);
+    console.log("Error en Servidor", error);
 })
