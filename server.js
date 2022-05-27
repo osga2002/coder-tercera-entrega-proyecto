48627ee18539659ac9801493b333bd50dbc30e3e
sistemaschoho <sistemas@chohocolombia.com>
2022-05-27 00:54:38 -0500

    Se continua con entrega del proyecto login error 2 poner cookies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const session = require('express-session')
+const session = require('cookie-session')
 //const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
